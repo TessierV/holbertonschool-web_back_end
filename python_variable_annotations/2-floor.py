5d2c70ec87d5cd70b164b7a0cdf3e786f6fb28d1
TessierV <5628@holbertonstudents.com>
2024-02-05 00:50:41 -0800

    ex 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+function floor take float as argument.
+:float n: argument
+:return: floor of the int
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function floor take float as argument.
+    :float n: argument
+    :return: floor of the int
+    """
+    return int(n)
