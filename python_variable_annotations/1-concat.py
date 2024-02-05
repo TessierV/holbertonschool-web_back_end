f6bb446f35db04ce12b52458ee61404b576b5f09
TessierV <5628@holbertonstudents.com>
2024-02-05 00:44:05 -0800

    ex 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Takes two string of type string as arguments.
+:string str1: First string
+:string str2: Second string
+:return: The sum of a and b of type string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two string of type string as arguments.
+    :string str1: First string
+    :string str2: Second string
+    :return: The sum of a and b of type string
+    """
+    return str1 + str2
