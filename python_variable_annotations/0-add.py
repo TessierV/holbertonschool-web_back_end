076c2056a94ed5e7c77aa463b35362469493c903
TessierV <5628@holbertonstudents.com>
2024-02-05 00:38:18 -0800

    fix documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -8,4 +8,10 @@ Takes two numbers of type float as arguments.
 
 
 def add(a: float, b: float) -> float:
+    """
+    Takes two numbers of type float as arguments.
+    :param a: First number of type float
+    :param b: Second number of type float
+    :return: The sum of a and b of type float
+    """
     return a + b
