83b765dd28cddf652c14e372c62c0c8bd9377170
TessierV <5628@holbertonstudents.com>
2024-02-05 06:53:54 -0800

    ex 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+function measure_time
+:max_delay: int: 10
+:n: int
+:return: total_time / n
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """
+    function measure_time
+    :max_delay: int: 10
+    :n: int
+    :return: total_time / n
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+
+    return total_time / n
