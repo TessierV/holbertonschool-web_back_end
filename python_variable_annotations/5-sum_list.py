19b352e5a71d74644ceed4d2f7a48756c1b29b50
TessierV <5628@holbertonstudents.com>
2024-02-05 01:09:03 -0800

    ex 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function sum_list take list as argument.
+:input_list List: float
+:return: sum list
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    function sum_list take list as argument.
+    :input_list List: float
+    :return: sum list
+    """
+    return sum(input_list)
