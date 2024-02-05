22c4e00bce124589177c4855d6a8e4cfc09909fa
TessierV <5628@holbertonstudents.com>
2024-02-05 00:33:20 -0800

    ex 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This function takes two numbers of type float as arguments and returns the sum of these two numbers.
+:param a: First number of type float
+:param b: Second number of type float
+:return: The sum of a and b of type float
+"""
 
-def add(a : float, b : float):
+def add(a : float, b : float) -> float:
     return a + b
