849fd081c93cd7dd550e9d5498fffc74d48408e9
TessierV <5628@holbertonstudents.com>
2024-02-05 00:36:44 -0800

    fix documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Takes two numbers of type float as arguments and returns the sum of these two numbers.
+Takes two numbers of type float as arguments.
 :param a: First number of type float
 :param b: Second number of type float
 :return: The sum of a and b of type float
