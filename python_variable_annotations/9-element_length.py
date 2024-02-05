ef7a3a9837c886d6e3d24071d72d2910e49a3894
TessierV <5628@holbertonstudents.com>
2024-02-05 02:07:54 -0800

    ex 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function iterable
+:lst: float
+:return: values with the appropriate types
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    return values with the appropriate types
+    """
+    return [(i, len(i)) for i in lst]
