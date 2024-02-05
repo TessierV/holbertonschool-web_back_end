cd7128d0eaee1428f26b1b01b359dbbf938b77dc
TessierV <5628@holbertonstudents.com>
2024-02-05 06:10:27 -0800

    checker documented

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-import asyncio
-import random
-
 """
 function wait_random
 :max_delay: int: 10
 :return: delay in float
 """
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
