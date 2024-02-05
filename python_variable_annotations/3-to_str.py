704e2619ad7ca6b1ae9c15c2c1fc09ef2b429145
TessierV <5628@holbertonstudents.com>
2024-02-05 00:54:24 -0800

    ex 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+function to_str take float as argument.
+:float n: argument
+:return: to_str of the string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str take float as argument.
+    :float n: argument
+    :return: to_str of the string
+    """
+    return str(n)
