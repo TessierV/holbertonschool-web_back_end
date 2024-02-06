cb4ec1b4537d9282ac111c4b0f1fd5efeaebaefb
TessierV <5628@holbertonstudents.com>
2024-02-06 00:04:02 -0800

    documented

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,8 +7,10 @@ class BasicCache(BaseCaching):
     """ BasicCache class """
 
     def put(self, key, item):
+        """ Put """
         if key is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ Get """
         return self.cache_data.get(key, None)
