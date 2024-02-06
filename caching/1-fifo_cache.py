177dc3c33c48409e58ebaab34398a6c26257c578
TessierV <5628@holbertonstudents.com>
2024-02-06 00:30:32 -0800

    test

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+FIFOCache
+:return: cache_data
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache
+    :return: cache_data
+    """
+    def put(self, key, item):
+        """
+        Put
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = self.order[0]
+                del self.cache_data[discard]
+                del self.order[0]
+                print("DISCARD: {}".format(discard))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get
+        :return: cache_data
+        """
+        return self.cache_data.get(key, None)
