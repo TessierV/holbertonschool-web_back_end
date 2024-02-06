2c073ff0c96b969dab308c7277365f497315b34e
TessierV <5628@holbertonstudents.com>
2024-02-06 00:07:27 -0800

    documented

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-""" BasicCache """
+"""
+BasicCache
+:return: cache_data
+"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
-    """ BasicCache class """
-
+    """
+    BasicCache
+    :return: cache_data
+    """
     def put(self, key, item):
-        """ Put """
+        """
+        Put
+        """
         if key is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get """
+        """
+        Get
+        :return: cache_data
+        """
         return self.cache_data.get(key, None)
