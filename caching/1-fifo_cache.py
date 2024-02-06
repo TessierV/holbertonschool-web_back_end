288a623f7f7ea27e8baaa395ef541034aaeb81ea
TessierV <5628@holbertonstudents.com>
2024-02-06 00:41:49 -0800

    order

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -17,12 +17,12 @@ class FIFOCache(BaseCaching):
         Put
         """
         if key is not None and item is not None:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discard = self.order[0]
-                del self.cache_data[discard]
-                print("DISCARD: {}".format(discard))
             self.cache_data[key] = item
             self.order.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = self.order[0]
+            del self.cache_data[discard]
+            print("DISCARD: {}".format(discard))
 
     def get(self, key):
         """
