707a2f26b041173ca5d3f8b01ef3871592ea5087
TessierV <5628@holbertonstudents.com>
2024-02-19 01:52:09 -0800

    ex 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,6 @@ Main file
 from typing import List
 import re
 
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     for item in fields:
         message = re.sub(fr'{item}=.+?{separator}', f'{item}={redaction}{separator}', message)
