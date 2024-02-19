a1fa8cb6f32247101cd955e1ace5e8b352c1a371
TessierV <5628@holbertonstudents.com>
2024-02-19 01:49:41 -0800

    ex 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,25 +2,11 @@
 """
 Main file
 """
+from typing import List
 import re
-import logging
-
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(fr'({separator}|^)({"|".join(fields)})=[^{separator}]+', f'\\1\\2={redaction}', message)
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    for item in fields:
+        message = re.sub(fr'{item}=.+?{separator}', f'{item}={redaction}{separator}', message)
+    return message
