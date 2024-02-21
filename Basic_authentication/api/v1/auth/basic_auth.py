10dc2b14752bb0070d5733d34f8ce322a2f72ca5
TessierV <5628@holbertonstudents.com>
2024-02-21 07:37:48 -0800

    ex

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,21 @@ class BasicAuth(Auth):
 
         base64_part = authorization_header.split(" ")[1]
         return base64_part
+
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ Decode Base64 """
+        if (
+            base64_authorization_header is None
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except binascii.Error:
+            return None
