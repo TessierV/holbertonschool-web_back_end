09716b5110fad2e6efdfcad4be108b65e23f171d
TessierV <5628@holbertonstudents.com>
2024-02-21 08:31:50 -0800

    ex

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,9 @@
 """
 import base64
 import binascii
+from typing import TypeVar
 from api.v1.auth.auth import Auth
-
+from models.user import User
 
 class BasicAuth(Auth):
     """ Basic Authentication Class """
@@ -55,3 +56,25 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """ Get User Object """
+        if (
+            user_email is None
+            or not isinstance(user_email, str)
+            or user_pwd is None
+            or not isinstance(user_pwd, str)
+        ):
+            return None
+
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        if not found_users or not found_users[0].is_valid_password(user_pwd):
+            return None
+
+        return found_users[0]
