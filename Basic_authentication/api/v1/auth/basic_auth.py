e11c9de1a1aef0b02b28276d1aed4fa4421b6855
TessierV <5628@holbertonstudents.com>
2024-02-21 07:22:10 -0800

    ex

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
+""" Basic Authentication
+"""
 from api.v1.auth.auth import Auth
-from base64 import b64decode
-from flask import request
+
 
 class BasicAuth(Auth):
-    """ Basic authentication class """
-
-    def authorization_header(self, request=None) -> str:
-        """ Parse Authorization header for basic authentication """
-        if request is None or 'Authorization' not in request.headers:
-            return None
-
-        auth_header = request.headers.get('Authorization')
-        if not auth_header.startswith('Basic '):
-            return None
-
-        encoded_credentials = auth_header.split(' ')[1]
-        decoded_credentials = b64decode(encoded_credentials).decode('utf-8')
-        return decoded_credentials
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ Extract user information from basic authentication """
-        credentials = self.authorization_header(request)
-        if credentials is None:
-            return None
-
-        return user
+    """ Basic Authentication Class """
+    pass
