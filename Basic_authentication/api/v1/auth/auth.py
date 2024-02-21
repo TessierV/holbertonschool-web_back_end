4216e327bcda27c7683f02049dd1cb629777c6c9
TessierV <5628@holbertonstudents.com>
2024-02-21 05:47:27 -0800

    ex

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,12 @@ class Auth:
     """ Auth class to manage API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ authentication is required """
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        path_slash_tolerant = path.endswith('/') and path[:-1]
+
+        return path_slash_tolerant not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """
