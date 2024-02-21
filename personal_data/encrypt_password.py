f1dd4f574b13f054ef52273b3af08c774fa30d11
TessierV <5628@holbertonstudents.com>
2024-02-21 03:20:25 -0800

    test

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,18 +3,18 @@
 Main file
 """
 import bcrypt
+from typing import Union
 
-
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """
-    Hashes
+    Hashes a password using bcrypt
     """
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
 
-def is_valid(hashed_password, password):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """
-    Validates a password
+    Validates a password using bcrypt
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
