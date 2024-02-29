76f996b393135b1a34a888ab4a6b0c9156bd4f2b
TessierV <5628@holbertonstudents.com>
2024-02-29 06:36:13 -0800

    ex 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -65,3 +65,10 @@ class Auth:
             return None
         else:
             return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy session """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
