ecde9de69370fe427fbde77ce01e5967eb3ae0dd
TessierV <5628@holbertonstudents.com>
2024-02-29 08:10:55 -0800

    ex 17

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -82,3 +82,12 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    @app.route('/reset_password', methods=['POST'], strict_slashes=False)
+    def get_reset_password_token() -> str:
+        """ reset password """
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token(email)
+        if token is None or email is None:
+            abort(403)
+        return jsonify({"email": email, "reset_token": token}), 200
