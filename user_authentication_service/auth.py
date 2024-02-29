7cc69fe2bc4a99f5654589bb1cb040d8a0e2106d
TessierV <5628@holbertonstudents.com>
2024-02-29 03:16:39 -0800

    ex 9

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -16,6 +16,7 @@ def _generate_uuid() -> str:
     """ return a string representation of a new UUID """
     return str(uuid4())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
