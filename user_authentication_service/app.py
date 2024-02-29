9e53d59dfba1148423c0737e4bc2980f9dc48581
TessierV <5628@holbertonstudents.com>
2024-02-29 07:04:50 -0800

    ex 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -38,5 +38,16 @@ def login():
     return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Log out """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
