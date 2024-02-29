ca85261ab51ce5697dc706d0425927a09ee8f730
TessierV <5628@holbertonstudents.com>
2024-02-29 06:14:35 -0800

    ex 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
@@ -24,6 +24,18 @@ def users():
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Log in """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
