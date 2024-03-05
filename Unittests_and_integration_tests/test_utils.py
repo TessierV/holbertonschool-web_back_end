1a09b7d654b8663d4dfe55c8250f83e07d138785
TessierV <5628@holbertonstudents.com>
2024-03-05 04:24:08 -0800

    ragequit ex3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -60,6 +60,3 @@ class TestMemoize(unittest.TestCase):
             test_class.a_property
             test_class.a_property
             mockMethod.assert_called_once
-
-if __name__ == "__main__":
-    unittest.main()
