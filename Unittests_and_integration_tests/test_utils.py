a6c96cab84119dea7c78631ef5a8f2376a225f25
TessierV <5628@holbertonstudents.com>
2024-03-05 02:41:33 -0800

    test ex1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -15,6 +15,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, answer):
         self.assertEqual(access_nested_map(nested_map, path), answer)
 
+    @parameterized.expand([
+        ({}, ("a",), "Key not found: 'a'"),
+        ({"a": 1}, ("a", "b"), "Key not found: 'b'"),
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(error.exception), path[-1] )
+
 
 if __name__ == "__main__":
     unittest.main()
