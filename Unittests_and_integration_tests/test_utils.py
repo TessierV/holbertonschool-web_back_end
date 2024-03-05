17bbb5b7e4ef9539e3e8090e1fe15de4c68341c3
TessierV <5628@holbertonstudents.com>
2024-03-05 03:31:04 -0800

    test ex2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -26,6 +26,22 @@ class TestAccessNestedMap(unittest.TestCase):
 
         self.assertEqual(str(error.exception.args[0]), path[-1] )
 
+class TestGetJson(unittest.TestCase):
+    """ test json """
+    @patch('requests.get')
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
 
 if __name__ == "__main__":
     unittest.main()
