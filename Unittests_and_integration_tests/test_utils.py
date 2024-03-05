8255d7de4d0c85795c18b49d795b3f6426272125
TessierV <5628@holbertonstudents.com>
2024-03-05 04:34:26 -0800

    ragequit total

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" unit test """
+""" Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
@@ -7,44 +7,50 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ testcase """
+    """ TESTCASE """
+    """ to test the function for following inputs """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, answer):
+        """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), answer)
 
+    """  to test that a KeyError is raised for the following inputs """
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-
     def test_access_nested_map_exception(self, nested_map, path):
+        """ method to test that a KeyError is raised properly """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
 
-        self.assertEqual(str(error.exception.args[0]), path[-1] )
 
 class TestGetJson(unittest.TestCase):
-    """ test json """
+    """ TESTCASE """
+    """ to test the function for following inputs """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     @patch('test_utils.get_json')
     def test_get_json(self, test_url, test_payload, mock_get):
-        mock_get.json.return_value = test_payload
+        """ method to test that utils.get_json returns the expected result """
+        mock_get.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     """ TESTCASE """
-
     def test_memoize(self):
-
-
+        """ Test that when calling a_property twice, the correct result is
+            returned but a_method is only called once using assert_called_once
+        """
         class TestClass:
             """ class """
             def a_method(self):
@@ -55,12 +61,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 """ property """
                 return self.a_method()
-
         with patch.object(TestClass, "a_method") as mockMethod:
             test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            mockMethod.assert_called_once()
-
-if __name__ == "__main__":
-    unittest.main()
+            test_class.a_property
+            test_class.a_property
+            mockMethod.assert_called_once
