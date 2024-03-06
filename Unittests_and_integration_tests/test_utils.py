88bcf4a5dc3247827a8db100d4e9f36e71a8b700
TessierV <5628@holbertonstudents.com>
2024-03-06 02:38:11 -0800

    ex3 test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """ testcase """
     @parameterized.expand([
@@ -14,6 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, answer):
         self.assertEqual(access_nested_map(nested_map, path), answer)
+
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
@@ -21,28 +23,32 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(error.exception.args[0]), path[-1] )
+        self.assertEqual(str(error.exception.args[0]), path[-1])
+
+
 class TestGetJson(unittest.TestCase):
     """ test json """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    @patch('test_utils.get_json')
+    @patch('utils.get_json')
     def test_get_json(self, test_url, test_payload, mock_get):
         mock_get.json.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
+
+
 class TestMemoize(unittest.TestCase):
     """ TESTCASE """
-
     def test_memoize(self):
         class TestClass:
             """ class """
             def a_method(self):
-                """ method  """
+                """ method """
                 return 42
-            @memoize
+
+            @memoize  # Add the memoize decorator
             def a_property(self):
                 """ property """
                 return self.a_method()
@@ -52,6 +58,7 @@ class TestMemoize(unittest.TestCase):
             test_class.a_property()
             test_class.a_property()
             mockMethod.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
