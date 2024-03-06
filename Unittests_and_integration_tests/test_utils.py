1c2302143c4591849a75c39613a56b9d43225351
TessierV <5628@holbertonstudents.com>
2024-03-06 02:48:40 -0800

    ex3 test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -27,17 +27,16 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    """ test json """
+    """ TESTCASE """
+    """ to test the function for following inputs """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    @patch('utils.requests.get')
+    @patch('utils.get_json')
     def test_get_json(self, test_url, test_payload, mock_get):
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
+        """ method to test that utils.get_json returns the expected result """
+        mock_get.return_value.json.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
