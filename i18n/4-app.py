e7730f25a19ac59779a83e907c39c202bdfba3e0
TessierV <5628@holbertonstudents.com>
2024-03-07 03:47:00 -0800

    test

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/')
 def root():
     """ basic Flask app """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
