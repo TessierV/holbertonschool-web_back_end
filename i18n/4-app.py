e7c8b79054d1aca1cfc95fdd84bc0a8ca8ea8211
TessierV <5628@holbertonstudents.com>
2024-03-07 03:44:57 -0800

    test

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -20,6 +20,8 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ locale selector """
+    if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
+        return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
