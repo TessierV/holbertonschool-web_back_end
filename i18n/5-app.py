7673018b8919619f5540d886cb3b9965f2a8cfc6
TessierV <5628@holbertonstudents.com>
2024-03-08 00:20:05 -0800

    remove getext

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -2,7 +2,7 @@
 """ Basic Flask app """
 
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
