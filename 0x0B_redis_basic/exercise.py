0254c5f78312640b5aa6e9e57cd69b214cf3d195
TessierV <5628@holbertonstudents.com>
2024-03-21 01:32:30 -0700

    test ex1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -26,3 +26,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        value = self._redis
+        return value.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> str:
+        value = self._redis
+        return value.get(key, fn=int)
+
+    def _generate_key(self):
+        return str(uuid.uuid4())
