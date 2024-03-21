ad04f1f882ddf61a9c0589bcbf0eb5bd78588bcf
TessierV <5628@holbertonstudents.com>
2024-03-21 02:28:40 -0700

    test

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,7 @@
--- SQL script that creates a table users
+-- SQL script that creates a table users add country
 CREATE TABLE If NOT EXISTS `users` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
-  `name` VARCHAR(255)
+  `name` VARCHAR(255),
+  `country` enum('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
