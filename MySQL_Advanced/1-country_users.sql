a8eafd57ddbe9a72c0801f10a5c03ed543c16d92
TessierV <5628@holbertonstudents.com>
2024-03-21 02:29:40 -0700

    enum change

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,5 @@ CREATE TABLE If NOT EXISTS `users` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
-  `country` enum('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+  `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
