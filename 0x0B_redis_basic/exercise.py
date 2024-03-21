31a3e1d791b8f7d0a9d0b9284968bb71ace066ef
TessierV <5628@holbertonstudents.com>
2024-03-21 01:33:55 -0700

    test ex1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -27,7 +27,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None):
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
         value = self._redis.get(key)
         if value is None:
             return None
