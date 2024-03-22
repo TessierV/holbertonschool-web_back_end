eba60e0cd5affea7959078a2f09010181bbab48d
TessierV <5628@holbertonstudents.com>
2024-03-22 08:28:35 -0700

    test ex

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,23 +2,48 @@
 """
 Writing strings to Redis
 """
-from redis.client import Redis
+from typing import Union, Callable, Optional, Any
 import redis
-from typing import Union
 import uuid
+from functools import wraps
 
+
+def call_history(method: Callable) -> Callable:
+    """ store the history of inputs and outputs for a particular function """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
+
+def count_calls(method: Callable) -> Callable:
+    """ to count how many times methods of the Cache class are called """
+    key = method.__qualname__
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 class Cache:
     """
     Class Cache.
     """
     def __init__(self):
-        """
-        Init
-        """
+        """ Init """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store
@@ -27,30 +52,25 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> str:
-        """
-        Get
-        """
-        value = self._redis.get(key)
-        if value is None:
-            return None
-        if fn is not None:
-            return fn(value)
-        return value
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ convert the data back to the desired format """
+        val = self._redis.get(key)
+        return val if not fn else fn(val)
 
     def get_str(self, key: str) -> str:
-        """
-        Get string
-        """
+        """ automatically parametrize Cache.get to str """
         value = self._redis
         return value.get(key, fn=lambda d: d.decode("utf-8"))
 
-    def get_int(self, key: str) -> str:
-        """
-        Get int
-        """
-        value = self._redis
-        return value.get(key, fn=int)
+    def get_int(self, key: str) -> int:
+        """ Get int """
+        data = self._redis.get(key)
+        try:
+            data = int(value.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
 
     def _generate_key(self):
         return str(uuid.uuid4())
