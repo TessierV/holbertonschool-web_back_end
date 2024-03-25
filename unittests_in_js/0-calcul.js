de1028ff446d34b4a98531f6a22c33ed2f527c8e
TessierV <5628@holbertonstudents.com>
2024-03-25 03:04:16 -0700

    ex 0

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+// returns the sum of these two rounded numbers.
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+// Test Summ
+module.exports = calculateNumber;
+
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', function() {
+  it('should return the rounded sum of two numbers', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
