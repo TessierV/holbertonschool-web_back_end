1fe8f7f5f2c3c27877a55dca0b282352df843e31
TessierV <5628@holbertonstudents.com>
2024-03-25 05:07:05 -0700

    adjust negatif and nan

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -11,10 +11,12 @@ describe('calculateNumber', function() {
   it('should handle negative numbers correctly', function() {
     assert.strictEqual(calculateNumber(-1, 3), 2);
     assert.strictEqual(calculateNumber(1, -3), -2);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
   });
   it('should return NaN if one of the arguments is not a number', function() {
     assert.strictEqual(isNaN(calculateNumber('a', 3)), true);
     assert.strictEqual(isNaN(calculateNumber(1, 'b')), true);
-    assert.strictEqual(isNaN(calculateNumber('foo', 'bar')), true);
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
