80afced4633df2a318d05071c0987e08916a7ff4
TessierV <5628@holbertonstudents.com>
2024-03-26 03:08:38 -0700

    change app post 10

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 7865;
 
@@ -6,5 +7,17 @@ app.get('/', (req, res) => res.end('Welcome to the payment system'));
 app.get('/cart/:id(\\d+)', (req, res) => {
     const cartId = req.params.id;
     res.end(`Payment methods for cart ${cartId}`);
+});
+app.get('/available_payments', (req, res) => {
+    res.json({
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+        }
+    });
+});
+app.post('/login', (req, res) => {
+    const { userName } = req.body;
+    res.end(`Welcome ${userName}`);
 });
 app.listen(port, () => console.log(`API available on localhost port ${port}`));
