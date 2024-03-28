2401971ba84846b5f3480c7a3ef37e86b84ae215
TessierV <5628@holbertonstudents.com>
2024-03-28 04:19:06 -0700

    pep8

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -1,23 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'));
-app.get('/cart/:id(\\d+)', (req, res) => {
-    const cartId = req.params.id;
-    res.end(`Payment methods for cart ${cartId}`);
-});
+app.get('/', (req, res) => res.end('Welcome to the payment system'))
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`)
+})
 app.get('/available_payments', (req, res) => {
-    res.json({
-        payment_methods: {
-            credit_cards: true,
-            paypal: false
-        }
-    });
+  const obj = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(obj);
 });
 app.post('/login', (req, res) => {
-    const { userName } = req.body;
-    res.end(`Welcome ${userName}`);
+  res.end(`Welcome ${req.body.userName}`);
 });
 app.listen(port, () => console.log(`API available on localhost port ${port}`));
--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -1,64 +1,79 @@
 const request = require('request');
 const chai = require('chai');
 
-describe('basic integration testing', () => {
-  describe('GET /', () => {
-    it('endpoint GET /', (done) => {
-      request('http://localhost:7865', (error, response, body) => {
-        if (error) {
-          done(error);
-          return;
-        }
-        chai.expect(response.statusCode).to.equal(200);
-        chai.expect(body).to.equal('Welcome to the payment system');
-        done();
-      });
+
+describe('GET /', () => {
+  it('endpoint: GET /', (done) => {
+    const call = {
+      url: 'http://localhost:7865',
+      method: 'GET',
+    };
+    request(call, (error, response, body) => {
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
+});
 
-  describe('regex integration testing', () => {
-    describe('GET /cart/:id', () => {
-      it('endpoint: GET /cart/:id', (done) => {
-        const call = {
-          url: 'http://localhost:7865/cart/12',
-          method: 'GET',
-        };
-        request(call, (error, response, body) => {
-          chai.expect(response.statusCode).to.equal(200);
-          chai.expect(body).to.equal('Payment methods for cart 12');
-          done();
-        });
-      });
+
+describe('GET /cart/:id', () => {
+  it('endpoint: GET /cart/:id', (done) => {
+    const call = {
+      url: 'http://localhost:7865/cart/12',
+      method: 'GET',
+    };
+    request(call, (error, response, body) => {
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(body).to.equal('Payment methods for cart 12');
+      done();
     });
   });
+});
 
-  describe('GET /cart/:isNaN', () => {
-    it('endpoint: GET /cart/:isNaN', (done) => {
-      const call = {
-        url: 'http://localhost:7865/cart/anything',
-        method: 'GET',
-      };
-      request(call, (error, response, body) => {
-        chai.expect(response.statusCode).to.equal(404);
-        done();
-      });
+
+describe('GET /cart/:isNaN', () => {
+  it('endpoint: GET /cart/:isNaN', (done) => {
+    const call = {
+      url: 'http://localhost:7865/cart/anything',
+      method: 'GET',
+    };
+    request(call, (error, response, body) => {
+      chai.expect(response.statusCode).to.equal(404);
+      done();
     });
   });
+});
 
-  describe('POST /login', () => {
-    it('POST /login', (done) => {
-      const call = {
-        url: 'http://localhost:7865/login',
-        method: 'POST',
-        json: {
-          userName: 'Vanessa',
-        },
-      };
-      request(call, (error, response, body) => {
-        chai.expect(response.statusCode).to.equal(200);
-        chai.expect(body).to.equal('Welcome Vanessa');
-        done();
-      });
+describe('GET /available_payments', () => {
+  it('endpoint: GET /available_payments', (done) => {
+    const call = {
+      url: 'http://localhost:7865/available_payments',
+      method: 'GET',
+    };
+    request(call, (error, response, body) => {
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(body).to.equal(
+        '{"payment_methods":{"credit_cards":true,"paypal":false}}'
+      );
+      done();
+    });
+  });
+});
+
+describe('POST /login', () => {
+  it('POST /login', (done) => {
+    const call = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      json: {
+        userName: 'Vanessa',
+      },
+    };
+    request(call, (error, response, body) => {
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(body).to.equal('Welcome Vanessa');
+      done();
     });
   });
 });
